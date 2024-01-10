657327d916d24d318997c1193b196e6e57faa6e5
Eliyahvaknin <eliyahu.vaknin@post.runi.ac.il>
2024-01-10 14:07:09 +0200

    LoanCalc.java

--- LoanCalc.java
+++ LoanCalc.java
@@ -1,54 +1,45 @@
-  public class LoanCalc {
-
-    static double epsilon = 0.001;
+    static double endBalance;
     static int iterationCounter;
-    static double epsilon = 0.001; 
-    static int iterationCounter; 
 
-       public static void main(String[] args) {
+    public static void main(String[] args) {
 
-        double loan = 100000;
-        double rate = 0.05;
-        int n = 10;
+        loan = Double.parseDouble(args[0]); 
+        rate = Double.parseDouble(args[1]); 
+        n = Integer.parseInt(args[2]); 
+        epsilon = 0.001;
+        payment = 10000;
+
         System.out.println("Loan sum = " + loan + ", interest rate = " + rate + "%, periods = " + n);
-
         System.out.print("Periodical payment, using brute force: ");
         System.out.printf("%.2f", bruteForceSolver(loan, rate, n, epsilon));
         System.out.println();
         System.out.println("number of iterations: " + iterationCounter);
-
         System.out.print("Periodical payment, using bi-section search: ");
         System.out.printf("%.2f", bisectionSolver(loan, rate, n, epsilon));
         System.out.println();
         System.out.println("number of iterations: " + iterationCounter);
     }
+
     public static double bruteForceSolver(double loan, double rate, int n, double epsilon) {
-        while (endBalance(loan, rate, n, epsilon) > 0) {
-            epsilon += epsilon;
-@@ -25,7 +42,6 @@ public static double bisectionSolver(double loan, double rate, int n, double eps
-
-        return (low + high) / 2;
+        g = loan / n;
+        iterationCounter = 0;
+@@ -65,5 +46,23 @@ private static double endBalance(double loan, double rate, int n, double payment
+        }
+        return endBalance;
     }
-
-    private static double endBalance(double loan, double rate, int n, double payment) {
-        double balance = loan;
-        for (int i = 0; i < n; i++) {
-@@ -34,22 +50,4 @@ private static double endBalance(double loan, double rate, int n, double payment
-        return balance;
-    }
-
     public static void main(String[] args) {
 
-        double loan = 100000;
-        double rate = 0.05;
-        int n = 10;
-        System.out.println("Loan sum = " + loan + ", interest rate = " + rate + "%, periods = " + n);
+        loan = Double.parseDouble(args[0]); 
+        rate = Double.parseDouble(args[1]); 
+        n = Integer.parseInt(args[2]); 
+        epsilon = 0.001;
+        payment = 10000;
 
+        System.out.println("Loan sum = " + loan + ", interest rate = " + rate + "%, periods = " + n);
         System.out.print("Periodical payment, using brute force: ");
         System.out.printf("%.2f", bruteForceSolver(loan, rate, n, epsilon));
         System.out.println();
         System.out.println("number of iterations: " + iterationCounter);
-
         System.out.print("Periodical payment, using bi-section search: ");
         System.out.printf("%.2f", bisectionSolver(loan, rate, n, epsilon));
         System.out.println();
